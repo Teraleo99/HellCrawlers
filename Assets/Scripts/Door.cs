b6a58f7910dae2c67eed8e40395930a555d946de
Teraleo99 <43067374+Teraleo99@users.noreply.github.com>
2019-01-18 01:50:48 +0000

    Alpha test game
    High scores complete, extra assets added for aesthetic purposes, might need a roof

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//extra using statement, for scene management functions
+using UnityEngine.SceneManagement;
+public class Door : MonoBehaviour
+{
+
+    //Designer variables
+    //variable to let us save the score, public so we can drag and drop
+    public Score scoreObject;
+    public string sceneToLoad;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //check if thing we collided with is player (aka has player script)
+        Player playerScript = collision.collider.GetComponent<Player>();
+
+        //only do this if thing ran into is player
+        if (playerScript != null)
+        {
+            //we did hit player!
+            //save score using our score object reference
+            scoreObject.SaveScore();
+            //Load next level
+            SceneManager.LoadScene(sceneToLoad);
+
+        }
+    }
+}
